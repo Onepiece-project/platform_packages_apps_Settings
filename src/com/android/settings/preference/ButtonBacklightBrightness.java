0139245e0907bfbbb2bb2820c84fd2945358ca9a
Zhao Wei Liew <zhaoweiliew@gmail.com>
2017-01-16 19:11:23 +0800

    ButtonBacklight: Preview timeout once user stops dragging
    Change-Id: I0abebe1041b6ad42b10e7d24f76ea17046e64666
    
    Conflicts:
    	src/com/android/settings/preference/ButtonBacklightBrightness.java

--- src/com/android/settings/preference/ButtonBacklightBrightness.java
+++ src/com/android/settings/preference/ButtonBacklightBrightness.java
@@ -1,6 +1,7 @@
 /*
  * Copyright (C) 2013 The CyanogenMod Project
- * Copyright (C) 2015 The JDCTeam
+ * Copyright (C) 2017 The LineageOS Project
+ * Copyright (C) 2015-2017 The JDCTeam
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -53,6 +54,8 @@ public class ButtonBacklightBrightness extends DialogPreference implements
 
     private ContentResolver mResolver;
 
+    private int mOriginalTimeout;
+
     public ButtonBacklightBrightness(Context context, AttributeSet attrs) {
         super(context, attrs);
 
@@ -92,7 +95,8 @@ public class ButtonBacklightBrightness extends DialogPreference implements
         mTimeoutValue = (TextView) view.findViewById(R.id.timeout_value);
         mTimeoutBar.setMax(30);
         mTimeoutBar.setOnSeekBarChangeListener(this);
-        mTimeoutBar.setProgress(getTimeout());
+        mOriginalTimeout = getTimeout();
+        mTimeoutBar.setProgress(mOriginalTimeout);
         handleTimeoutUpdate(mTimeoutBar.getProgress());
 
         ViewGroup buttonContainer = (ViewGroup) view.findViewById(R.id.button_container);
@@ -117,6 +121,7 @@ public class ButtonBacklightBrightness extends DialogPreference implements
             @Override
             public void onClick(View v) {
                 mTimeoutBar.setProgress(DEFAULT_BUTTON_TIMEOUT);
+                applyTimeout(DEFAULT_BUTTON_TIMEOUT);
                 if (mButtonBrightness != null) {
                     mButtonBrightness.reset();
                 }
@@ -130,6 +135,7 @@ public class ButtonBacklightBrightness extends DialogPreference implements
         super.onDialogClosed(positiveResult);
 
         if (!positiveResult) {
+            applyTimeout(mOriginalTimeout);
             return;
         }
 
@@ -249,7 +255,7 @@ public class ButtonBacklightBrightness extends DialogPreference implements
 
     @Override
     public void onStopTrackingTouch(SeekBar seekBar) {
-        // Do nothing here
+        applyTimeout(seekBar.getProgress());
     }
 
     private static class SavedState extends BaseSavedState {
